efdb6252ab088783db22e301b2a648650ee7f13a
harsaihaj <harsaihaj.gill@gmail.com>
2025-01-30 00:11:38 +0530

    thode tweaks

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -8,72 +8,96 @@ class loginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    body: Container(
-      alignment: Alignment.center,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
         children: [
-          Text("Welcome To"),
-            Text("TRIPLY", style: GoogleFonts.dmSerifText(
-                textStyle: TextStyle(fontSize: 70, fontWeight: FontWeight.w500, color: Color(0xff4966F0))
-            ),),
-
-          SizedBox(height: 30,),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 30),
-
-            child: TextField(
-              decoration: InputDecoration(
-                icon: Icon(Icons.alternate_email_rounded),
-                labelText: 'Thapar Mail ID',
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  borderSide: const BorderSide(color: Colors.black38),
-
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  borderSide: const BorderSide(color: Colors.black38),
-                )
-              ),
-            ),
-          ),
-          SizedBox(height: 20,),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 30),
-            child: TextField(
-              decoration: InputDecoration(
-                  icon: Icon(Icons.lock_open_rounded),
-                  labelText: 'Password',
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(color: Colors.black38),
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text("Welcome To"),
+                Text(
+                  "TRIPLY",
+                  style: GoogleFonts.dmSerifText(
+                    textStyle: const TextStyle(
+                      fontSize: 70,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xff4966F0),
+                    ),
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(color: Colors.black38),
-                  )
+                ),
+                const SizedBox(height: 30),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 30),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      icon: const Icon(Icons.alternate_email_rounded),
+                      labelText: 'Thapar Mail ID',
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: const BorderSide(color: Colors.black38),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: const BorderSide(color: Colors.black38),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 30),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      icon: const Icon(Icons.lock_open_rounded),
+                      labelText: 'Password',
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: const BorderSide(color: Colors.black38),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: const BorderSide(color: Colors.black38),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                SizedBox(
+                  width: MediaQuery.sizeOf(context).width / 2,
+                  height: MediaQuery.sizeOf(context).width / 8,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xff4966F0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    child: const Text(
+                      "Login",
+                      style: TextStyle(fontSize: 15, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 40,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text("Don't Have an Account?"),
+                  TextButton(onPressed: (){}, child: Text("Register Here"))
+                ],
               ),
             ),
           ),
-          SizedBox(height: 30,),
-          Container(
-            width: MediaQuery.sizeOf(context).width/2,
-            height: MediaQuery.sizeOf(context).width/8,
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xff4966F0),
-                // Text color
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12), // Rounded corners
-                ),
-              ),
-              child: Text("Login", style: TextStyle(fontSize: 15,color: Colors.white, ),),
-            ),
-          )          ],
+        ],
       ),
-    ),
     );
   }
 }
